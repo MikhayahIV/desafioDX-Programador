f19413662593b14be2f840b8c7ce73c8deea0305
MikhayahIV <sasha.vessa@gmail.com>
2026-09-04 15:51:06 -0300

    feat: implemente Repositories

---
+++ src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
@@ -0,0 +1,13 @@
+package br.com.duxusdesafio.repository;
+
+import br.com.duxusdesafio.model.Time;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Repository
+public interface TimeRepository extends JpaRepository<Time, Long> {
+    Optional<Time> findByData(LocalDate data);
+}
