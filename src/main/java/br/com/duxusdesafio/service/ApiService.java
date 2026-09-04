c26789f44ea634047503c86d06f7cd7ecab6a5ee
MikhayahIV <sasha.vessa@gmail.com>
2026-09-04 15:52:39 -0300

    feat: implement timeDaData, integranteMaisUsado, clubeMaisRecorrente

--- src/main/java/br/com/duxusdesafio/service/ApiService.java
+++ src/main/java/br/com/duxusdesafio/service/ApiService.java
@@ -1,10 +1,17 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.ComposicaoTime;
 import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.model.Time;
+import br.com.duxusdesafio.repository.CompTimeRepository;
+import br.com.duxusdesafio.repository.IntegranteRepository;
+import br.com.duxusdesafio.repository.TimeRepository;
+import javassist.NotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -20,12 +27,26 @@ import java.util.Map;
 @Service
 public class ApiService {
 
+    private final CompTimeRepository compTimeRepository;
+    private final IntegranteRepository integranteRepository;
+    private final TimeRepository timeRepository;
+
+    public ApiService(CompTimeRepository compTimeRepository, IntegranteRepository integranteRepository, TimeRepository timeRepository) {
+        this.compTimeRepository = compTimeRepository;
+        this.integranteRepository = integranteRepository;
+        this.timeRepository = timeRepository;
+    }
+
     /**
      * Vai retornar um Time, com a composição do time daquela data
      */
-    public Time timeDaData(LocalDate data, List<Time> todosOsTimes){
+    public Time timeDaData(LocalDate data, List<Time> todosOsTimes) throws NotFoundException {
         // TODO Implementar método seguindo as instruções!
-        return null;
+        /*return timeRepository.findByData(data)
+                .orElseThrow(() ->  new NotFoundException("Nenhum time com a data informada")); */
+        return todosOsTimes.stream()
+                .filter(time -> time.getData().equals(data))
+                .findFirst().orElseThrow(() ->  new NotFoundException("Nenhum time com a data informada"));
     }
 
     /**
@@ -34,7 +55,21 @@ public class ApiService {
      */
     public Integrante integranteMaisUsado(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
         // TODO Implementar método seguindo as instruções!
-        return null;
+        Map<Integrante, Integer> contagem = new HashMap<>();
+        for (Time time : todosOsTimes){
+            if (!time.getData().isBefore(dataInicial)&& !time.getData().isAfter(dataFinal)){
+                for(ComposicaoTime composicaoTime : time.getComposicaoTime()){
+                    Integrante integrante = composicaoTime.getIntegrante();
+                    contagem.merge(integrante,1,Integer::sum);
+                }
+            }
+        }
+        return contagem.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null)
+                ;
     }
 
     /**
@@ -59,7 +94,20 @@ public class ApiService {
      */
     public String clubeMaisRecorrente(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes) {
         // TODO Implementar método seguindo as instruções!
-        return null;
+
+        Map<String, Integer> contagemClubes = new HashMap<>();
+        for(Time time: todosOsTimes){
+            if(!time.getData().isBefore(dataInicial)&&!time.getData().isAfter(dataFinal)){
+                String clube = time.getNomeDoClube();
+
+                contagemClubes.merge(clube,1,Integer::sum);
+            }
+        }
+        return contagemClubes.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
     }
 
 
