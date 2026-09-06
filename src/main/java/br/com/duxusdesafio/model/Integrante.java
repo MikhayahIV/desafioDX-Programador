ad5ab13a22405e22c204c55ecf9a7b05c2951ba2
MikhayahIV <sasha.vessa@gmail.com>
2026-09-06 12:02:40 -0300

    refactor: change getters and setters types from primivite long to Long wrapper

--- src/main/java/br/com/duxusdesafio/model/Integrante.java
+++ src/main/java/br/com/duxusdesafio/model/Integrante.java
@@ -34,11 +34,11 @@ public class Integrante {
 		this.composicaoTime = composicaoTime;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
--- src/main/java/br/com/duxusdesafio/model/Time.java
+++ src/main/java/br/com/duxusdesafio/model/Time.java
@@ -45,11 +45,11 @@ public class Time {
 		this.composicaoTime = composicaoTime;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
